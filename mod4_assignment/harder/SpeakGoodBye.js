248368b3b680637ea4e7ab69e01fd89a94974c32
Satish K R <61915099+SaTiSH-K-R@users.noreply.github.com>
2020-06-29 18:04:52 +0530

    Add files via upload

---
+++ mod4_assignment/harder/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function (window) {
+  var speakWord = "Good Bye";
+  byeSpeaker = {}
+  byeSpeaker.speak = function (name) {
+      console.log(speakWord + " " + name);
+  }
+  window.byeSpeaker = byeSpeaker;
+})(window);
